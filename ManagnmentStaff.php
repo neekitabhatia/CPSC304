19dc92b09de1e1819ac8a88b6fe8ab56074a560d
CatherineSiu <catherinewysiu@gmail.com>
2018-03-15 14:55:07 -0700

    general and management and index

---
+++ ManagnmentStaff.php
@@ -0,0 +1,16 @@
+<?php include "template/header.php"; ?>
+<html>
+<h2>Managnment Staff</h2>
+<h3>Login</h3>
+
+<form method="post">
+	<label for="Login">Login</label>
+	<input type="text" name="Login" id="Login">
+	<label for="Password">Password</label>
+	<input type="text" name="Password" id="Password">
+	<input type="submit" name="submit" value="Submit">
+</form>
+
+<html>
+
+<?php include "template/footer.php"; ?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include "template/header.php"; ?>
+
+<ul>
+<a href="public.php"><strong>Public</strong></a>
+<br>
+<a href="GeneralStaff.php"><strong>General Staff</strong></a>
+<br>
+<a href="ManagnmentStaff.php"><strong>Management Staff</strong></a> 
+</ul>
+
+<?php include "template/footer.php"; ?>
