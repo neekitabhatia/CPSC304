545cb5ae31199ea7f89e1872119c2536e233cacc
neekitabhatia <neekitabhatia@gmail.com>
2018-03-15 13:47:18 -0700

    2

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+
+if ($c=OCILogon("ora_c5y0b", "a36856169", "dbhost.ugrad.cs.ubc.ca:1522/ug")) {
+  echo "Successfully connected to Oracle.\n";
+  OCILogoff($c);
+} else {
+  $err = OCIError();
+  echo "Oracle Connect Error " . $err['message'];
+}
+
+?>
+</html>
