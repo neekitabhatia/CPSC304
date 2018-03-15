628281cfd5df758fe917416fce9b302aa139925e
nikib <neekitabhatia@gmail.com>
2018-03-15 14:29:25 -0700

    .2

--- hello.php
+++ hello.php
@@ -1,7 +1,7 @@
 <html>
 <p>If PHP is working, you will see "Hello World" below:<hr>
 <?php
-   echo "Hello world foo";
+   echo "Hello world f";
    phpinfo();  // Print PHP version and config info
 ?>
 
