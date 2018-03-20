45819d38e2544e442bcf482655cefc6f4dedf1f8
CatherineSiu <catherinewysiu@gmail.com>
2018-03-20 13:00:43 -0700

    2.0
    Changed to Design

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "template/header.php"; ?>
 
 <ul>
-<a href="public.php"><strong>Public</strong></a>
+<a href="Public.php"><strong>Public</strong></a>
 <br>
 <a href="GeneralStaff.php"><strong>General Staff</strong></a>
 <br>
