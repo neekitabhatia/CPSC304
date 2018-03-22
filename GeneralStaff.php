5ba817d94bfe133d4f40bb4a420b4c9f7d2f6bc3
nikib <neekitabhatia@gmail.com>
2018-03-22 12:49:48 -0700

    1.1

--- GeneralStaff.php
+++ GeneralStaff.php
@@ -1,9 +1,38 @@
-<?php include "template/header.php"; ?>
+<?php 
+include "template/header.php";
+include "config.php"; # for db connectivity info
+
+// if($_SERVER["REQUEST_METHOD"] == "POST") {
+//       // username and password sent from form 
+      
+//       $myusername = mysqli_real_escape_string($db,$_POST['username']);
+//       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+//       $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+//       $result = mysqli_query($db,$sql);
+//       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+//       $active = $row['active'];
+      
+//       $count = mysqli_num_rows($result);
+      
+//       // If result matched $myusername and $mypassword, table row must be 1 row
+		
+//       if($count == 1) {
+//          session_register("myusername");
+//          $_SESSION['login_user'] = $myusername;
+         
+//          header("location: welcome.php");
+//       }else {
+//          $error = "Your Login Name or Password is invalid";
+//       }
+//    }
+?>
+
 <html>
 <h2>General Staff</h2>
 <h3>Login</h3>
 
-<form method="post">
+<form method="post" action="GeneralStaff.php">
 	<label for="Login">Login</label>
 	<input type="text" name="Login" id="Login">
 	<label for="Password">Password</label>
