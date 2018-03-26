24f945cff79c256919f82ed59d19352d140a26eb
CatherineSiu <37420052+CatherineSiu@users.noreply.github.com>
2018-03-25 20:58:50 -0700

    with queries updated (doesnt print)

--- Public.php
+++ Public.php
@@ -1,33 +1,86 @@
 <?php include "template/header.php"; 
-include "install.php";
-if ($db_conn) {
-	$RecreationCenter = $_POST['RecreationCenter'];
-	echo $RecreationCenter;
-	$result = executePlainSQL("Select * from recreation_center where rc_name = '" . $RecreationCenter . "'");
-	printResult($result) ;	
-}
-
-
 ?>
 <html>
 <h2>Look for Recreational Programs</h2>
 <h3>Search by</h3>
 
-<form method="post">
+<form method="POST" action="Public.php">
 	<label for="Recreation Center">Recreation Center</label>
-	<input type="text" name="RecreationCenter">
-	<label for="Event">Event</label>
-	<input type="text" name="Event" id="Event">
-	<label for="Cost Range">Cost range</label>
-	<input type="integer" name="costmin" >
-	<input type="integer" name="costmax" >
+	<input type="text" name="recname">
+	<label for="Event">Event Name</label>
+	<input type="text" name="ename">
+    <label for="Cost Range">Cost range</label>
+	<input type="integer" name="cmin" >
+	<input type="integer" name="cmax" >
 	<label for="Time Range">Time range</label>
-	<input type="integer" name="timemin" >
-	<input type="integer" name="timemax" >
-	<input type="submit" name="submit" value="Submit">
+	<input type="integer" name="tmin" >
+	<input type="integer" name="tmax" >  
+	<input type="submit" name="submit" value="submit">
 </form>
+<br> 
+<center>
+<?php 
+include "install.php";
 
+if (array_key_exists('submit', $_POST)) {
+ 	$recname = $_POST['recname'];
+    $ename = $_POST['ename'];
+    $cmin = $_POST['cmin'];
+    $cmax = $_POST['cmax'];
+    $tmin = $_POST['tmin'];
+    $tmax = $_POST['tmax'];
+
+    $string = "SELECT recreation_center.rc_name " .
+             "FROM recreation_center, facilities_contains, event_booking " .
+              "WHERE recreation_center.rc_name = facilities_contains.rc_name AND facilities_contains.fc_room_id = event_booking.fc_room_id ";
+
+              //   "FROM recreation_center " .
+              // "INNER JOIN facilities_contains ON recreation_center.rc_name = facilities_contains.rc_name " . 
+              // "INNER JOIN event_booking ON facilities_contains.fc_room_id = event_booking.fc_room_id " .
+              // "WHERE ";
+
+    
+    if (!empty($recname)){
+                $string .= "AND recreation_center.rc_name = '" . $recname . "'";
+                }
+
+    if (!empty($ename)){
+                $string .= "AND eb_name = '" . $ename . "'";
+                }
+
+    if (!empty($cmin)){
+                $string .= "AND eb_cost > '" . $cmin . "'";
+                }
+
+    if (!empty($cmax)){
+                $string .= "AND eb_cost < '" . $cmax . "'";
+                }
+
+    if (!empty($tmin)){
+                $string .= "AND eb_time_in > '" . $tmin . "'";
+                }
+
+    if (!empty($tmax)){
+                $string .= "AND eb_time_in < '" . $tmax . "'";
+                }
+
+    $result = executePlainSQL($string);
+
+    while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+             echo $row[0]; // it wont print :(
+             echo $row[1]; // it wont print :(
+
+          }
+//     while (($row = oci_fetch_assoc($result)) != false) {
+//     echo $row['DEPARTMENT_ID'] . " " . $row['DEPARTMENT_NAME'] . "<br>\n";
+// }
+   }
+  
+?>
+</center>
+<br> 
 <a href="index.php">Back</a>
+<br> 
 <html>
 
 <?php include "template/footer.php"; ?>
