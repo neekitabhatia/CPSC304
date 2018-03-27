69f948e37c93c6ce225df2a4f56d555125c84b7a
CatherineSiu <37420052+CatherineSiu@users.noreply.github.com>
2018-03-26 18:52:58 -0700

    Done

--- install.php
+++ install.php
@@ -1,38 +1,37 @@
 <?php
-
+ 
 /**
- * Open a connection 
+ * Open a connection
  *
  */
-
+ 
 $success = True; //keep track of errors so it redirects the page only if there are no errors
-$db_conn = OCILogon('ora_c5y0b', "a36856169", "dbhost.ugrad.cs.ubc.ca:1522/ug");
-
+$db_conn = OCILogon('ora_m2m0b', "a15378152", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+ 
 function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-	//echo "<br>running ".$cmdstr."<br>";
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
-		// connection handle
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	$r = OCIExecute($statement, OCI_DEFAULT);
-	if (!$r) {
-		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-		echo htmlentities($e['message']);
-		$success = False;
-	} else {
-
-	}
-	return $statement;
-
+    //echo "<br>running ".$cmdstr."<br>";
+    global $db_conn, $success;
+    $statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+ 
+    if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn); // For OCIParse errors pass the
+        // connection handle
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+ 
+    $r = OCIExecute($statement, OCI_DEFAULT);
+    if (!$r) {
+        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+        $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+        echo htmlentities($e['message']);
+        $success = False;
+    } else {
+ 
+    }
+    return $statement;
+ 
 }
-
-
+ 
 ?>
